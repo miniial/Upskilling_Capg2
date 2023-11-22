7d91626fb7bebd04362f0b554c6e61dfde0fcfa5
minyadav <minyadav@LIN24006869.corp.capgemini.com>
2023-11-22 19:41:50 +0530

    initial commit

---
+++ StudentDetails/src/main/java/com/Task1/StudentDetails/Service/StudentService.java
@@ -0,0 +1,39 @@
+package com.Task1.StudentDetails.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Task1.StudentDetails.Model.StudentDetails;
+import com.Task1.StudentDetails.Repo.StudentRepo;
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentRepo sr;
+	
+	public StudentDetails createStudentInfo(StudentDetails s) {
+		
+		
+		 return	sr.save(s);
+		}
+	
+	public List<StudentDetails> getAllStudent() {
+		// TODO Auto-generated method stub
+		List<StudentDetails> s=sr.findAll();
+		if(s.size()>0) {
+			return s;
+		}
+		else {
+			return new ArrayList<StudentDetails>();
+		}
+	}
+	
+
+	}
+
+
+
